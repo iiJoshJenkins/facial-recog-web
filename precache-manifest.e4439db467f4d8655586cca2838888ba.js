dd02dfc5856a527589cb3142cfb6776e6c168640
Josh Jenkins <joshua.jenkins@live.co.uk>
2019-07-24 12:34:04 +0100

    Updates

--- precache-manifest.e4439db467f4d8655586cca2838888ba.js
+++ precache-manifest.e4439db467f4d8655586cca2838888ba.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2c09ddbb2f85fa7e71ae3e74c3d90d65",
+    "revision": "9cdf0e0720fb0caef25a6f51ab383885",
     "url": "/facial-recog-web/index.html"
   },
   {
-    "revision": "3e52901564bb89d798c2",
-    "url": "/facial-recog-web/static/css/main.0c0a3989.chunk.css"
+    "revision": "75dcc83b4a64d81aff66",
+    "url": "/facial-recog-web/static/css/main.e474ff11.chunk.css"
   },
   {
     "revision": "d82afacfb8046780bdff",
     "url": "/facial-recog-web/static/js/2.c272f57a.chunk.js"
   },
   {
-    "revision": "3e52901564bb89d798c2",
-    "url": "/facial-recog-web/static/js/main.e7275afb.chunk.js"
+    "revision": "75dcc83b4a64d81aff66",
+    "url": "/facial-recog-web/static/js/main.3cbeb8c7.chunk.js"
   },
   {
     "revision": "4ccd1363090a38cced0d",
     "url": "/facial-recog-web/static/js/runtime~main.ee0c5f77.js"
+  },
+  {
+    "revision": "940ace70d6c766e685a20dea33762abf",
+    "url": "/facial-recog-web/static/media/American-Typewriter-Regular.940ace70.ttf"
   },
   {
     "revision": "feff963fc8b3ade0dc1d8413a44a476b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/facial-recog-web/precache-manifest.ef3752fc9ab66bade5b5937f50990042.js"
+  "/facial-recog-web/precache-manifest.e4439db467f4d8655586cca2838888ba.js"
 );
 
 self.addEventListener('message', (event) => {
