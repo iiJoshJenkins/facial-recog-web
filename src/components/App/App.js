5879c944cc17a4469f4f7782057ff719f1aa8bde
Josh Jenkins <joshua.jenkins@live.co.uk>
2019-07-24 11:22:58 +0100

    added age and gender parsing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,24 +2,35 @@ import React, { Component } from "react";
 
 export default class App extends Component {
   render() {
+    document.body.style.backgroundColor = "#000";
     const params = window.location.search.split("&");
-    const sortedParams = {};
+    const sortedParams = {
+      gender: null,
+      age: null
+    };
     params.forEach(param => {
       let _param = param;
       if (/^[?]/g.test(_param)) {
         _param = _param.substr(1);
       }
       _param = _param.split("=");
-      sortedParams[_param[0]] = _param[1];
+      if (_param[1] === "1") {
+        if (/s$/g.test(_param[0])) {
+          _param[0] = _param[0].substr(0, _param[0].length - 1);
+        }
+        if (_param[0] === "male" || _param[0] === "female") {
+          sortedParams.gender = _param[0];
+        } else {
+          sortedParams.age = _param[0];
+        }
+      }
     });
+
     return (
       <div>
-        {Object.keys(sortedParams).map(item => (
-          <li>
-            {" "}
-            {item} : {sortedParams[item]}{" "}
-          </li>
-        ))}
+        <h1 style={{ color: "white" }}>
+          You are a {sortedParams.age} {sortedParams.gender}
+        </h1>
       </div>
     );
   }
